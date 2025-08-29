5aa1a134db3ec7aeba08c3cd0b9e54e86b294590
T-H-A-N-N <151850072+T-H-A-N-N@users.noreply.github.com>
2025-08-29 05:01:42 +0200

    Update review.js

--- backend/src/models/review.js
+++ backend/src/models/review.js
@@ -3,27 +3,29 @@ import sequelize from "./index.js";
 import { User } from "./user.js";
 import { Book } from "./book.js";
 
-export const Review = sequelize.define("Review", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  rating: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    validate: {
-      min: 1,
-      max: 10,
+export default function(sequelize) => {
+  const sequelize.define('Review', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-  },
-  comment: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-});
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 10,
+      },
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  }, { 
+    tableName: 'reviews',
+    timestamps: false,
+  });
+  return Book;
+}
 
-User.hasMany(Review, { foreignKey: "userId" });
-Review.belongsTo(User, { foreignKey: "userId" });
-Book.hasMany(Review, { foreignKey: "bookId" });
-Review.belongsTo(Book, { foreignKey: "bookId" });
